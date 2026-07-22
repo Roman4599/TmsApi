a21e3a8f058b78d3c3765be725a0dadddf0663f4
Roman4599 <mengestaroman@gmail.com>
2026-07-22 13:02:48 -0700

    Lab 4 Exercise 1

--- obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abb2bc9d8fbb6f0b875bc9419a22d3e997059a04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab95a7ab786d08a806e4c95f1bd1eee7d8cba536")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
