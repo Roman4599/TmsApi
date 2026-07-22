2f1519ca9321bf175416aea8aa135760320f6c69
Roman4599 <mengestaroman@gmail.com>
2026-07-22 14:01:18 -0700

    Implement custom request logging middleware EXERCISE 1B

--- Program.cs
+++ Program.cs
@@ -22,6 +22,8 @@ app.Run();*/
 
 
 // lab 4 exercise 1
+
+/*
 var builder = WebApplication.CreateBuilder(args);
 
 // Add training authentication + authorization
@@ -29,6 +31,7 @@ builder.Services.AddAuthentication("Training").AddScheme <AuthenticationSchemeOp
 (
         "Training",
         null);
+        
 
 builder.Services.AddAuthorization();
 
@@ -52,4 +55,37 @@ app.MapGet("/api/assessments/results", () =>
 
 .RequireAuthorization();
 
+app.Run();
+*/
+var builder = WebApplication.CreateBuilder(args);
+// --- Add required services ---
+builder.Services.AddAuthentication("Training")
+    .AddScheme<AuthenticationSchemeOptions, TrainingAuthHandler>("Training", null);
+builder.Services.AddAuthorization();
+// --- End services ---
+
+
+var app = builder.Build();
+
+app.UseMiddleware<RequestLoggingMiddleware>();
+
+app.UseExceptionHandler("/error");
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapGet("/api/assessments/results", () =>
+{
+    return Results.Ok(new
+    {
+        courseCode = "CS-101",
+        studentId = "S-001",
+        letterGrade = "A"
+    });
+}).RequireAuthorization();
 app.Run();
--- obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab95a7ab786d08a806e4c95f1bd1eee7d8cba536")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a21e3a8f058b78d3c3765be725a0dadddf0663f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
