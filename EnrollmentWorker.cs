66e367d92c7c885633339cc34b0316da04695108
Roman4599 <mengestaroman@gmail.com>
2026-07-23 08:13:06 -0700

    Exercise 2 Step B: Fix captive dependency with IServiceScopeFac

--- EnrollmentWorker.cs
+++ EnrollmentWorker.cs
@@ -1,6 +1,6 @@
 
 
-public class EnrollmentWorker
+/*public class EnrollmentWorker
 {
     //EnrollmentWorker keeps a reference to IEnrollmentService
         private readonly IEnrollmentService _enrollmentService;
@@ -15,3 +15,26 @@ public class EnrollmentWorker
         _enrollmentService.GetAllAsync();
     }
 }
+*/
+// lab 4Exercise 2 Step B: Fix captive dependency with IServiceScopeFactory
+
+public class EnrollmentWorker
+{
+    private readonly IServiceScopeFactory _scopeFactory;
+
+    public EnrollmentWorker(IServiceScopeFactory scopeFactory)
+    {
+        _scopeFactory = scopeFactory;
+    }
+
+    public async Task ProcessBatch()
+    {
+   
+    using var scope = _scopeFactory.CreateScope();
+
+    var svc = scope.ServiceProvider.GetRequiredService<IEnrollmentService>();
+    await svc.GetAllAsync();
+    }
+}
+
+ 
